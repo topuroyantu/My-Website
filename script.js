23a9e8842df26350175a76ffd3875b76309acb16
Topu Roy Antu <84620484+topuroyantu@users.noreply.github.com>
2021-07-28 00:04:24 +0600

    Site

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// Sticky Menu
+
+let nav = document.querySelector("nav")
+
+window.addEventListener("scroll", function (){
+    nav.classList.toggle("sticky", window.scrollY >0)
+})
+
+
+
+
+
+// Responsive Menu
+
+let openBTN = document.getElementById("open")
+let closeBTN = document.getElementById("close")
+let menu = document.querySelector(".menu")
+
+function openMenu(){
+    menu.style.right="0"
+}
+
+function closeMenu(){
+    menu.style.right="-160px"
+}
